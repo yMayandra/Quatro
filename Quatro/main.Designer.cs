06b940fcf8cccefdbaadfee618e948aa1d4904e3
mayandra.vmota <mayandra.vmota@SBC0505221W10-1>
2022-04-28 15:58:47 -0300

    Adicionar arquivos de projeto.

---
+++ Quatro/main.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace Quatro
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxNumero = new System.Windows.Forms.TextBox();
+            this.tnCalcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbxNumero
+            // 
+            this.tbxNumero.Location = new System.Drawing.Point(256, 68);
+            this.tbxNumero.Name = "tbxNumero";
+            this.tbxNumero.Size = new System.Drawing.Size(100, 23);
+            this.tbxNumero.TabIndex = 0;
+            // 
+            // tnCalcular
+            // 
+            this.tnCalcular.Location = new System.Drawing.Point(379, 68);
+            this.tnCalcular.Name = "tnCalcular";
+            this.tnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.tnCalcular.TabIndex = 3;
+            this.tnCalcular.Text = "Clacular";
+            this.tnCalcular.UseVisualStyleBackColor = true;
+            this.tnCalcular.Click += new System.EventHandler(this.tnCalcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(256, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Digite um número";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.lblResultado.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblResultado.Location = new System.Drawing.Point(256, 118);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(198, 65);
+            this.lblResultado.TabIndex = 5;
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tnCalcular);
+            this.Controls.Add(this.tbxNumero);
+            this.KeyPreview = true;
+            this.Name = "main";
+            this.Text = "main";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.main_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxNumero;
+        private System.Windows.Forms.Button tnCalcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
---
+++ Quatro/main.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quatro
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tnCalcular_Click(object sender, EventArgs e)
+        {
+            if (tbxNumero.Text == "")
+            {
+                MessageBox.Show("Insira um Número");
+            }
+            else
+            {
+                int numero = 0;
+                int quadrado = 0;
+                numero = Convert.ToInt32(tbxNumero.Text);
+                quadrado = numero * numero;
+                lblResultado.Text ="O número "+ numero +" ao quadrado é " +  quadrado.ToString();
+                tbxNumero.Focus();
+                tbxNumero.SelectAll();
+            }
+            
+        }
+
+        private void main_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+            if (e.KeyChar==13) 
+            {
+                tnCalcular.PerformClick();
+            }
+        }
+    }
+}
